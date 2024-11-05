ac772f027f8c6ee969b105756d463da23dae3fed
someone <dzumushwana@gmail.com>
2024-11-05 15:15:02 +0200

    Initial commit

---
+++ src/main/java/com/project/start/repository/SearchInstitutionRepository.java
@@ -0,0 +1,17 @@
+package com.project.start.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.project.start.entity.Institution;
+
+@Repository
+public interface SearchInstitutionRepository extends JpaRepository<Institution, Integer>{
+
+	 @Query(value = "SELECT * FROM institutions s WHERE s.institutionname LIKE %:keyword% OR s.location LIKE %:keyword%",
+	            nativeQuery = true)
+    List<Institution> findByKeyword(@Param("keyword") String keyword);
+}
